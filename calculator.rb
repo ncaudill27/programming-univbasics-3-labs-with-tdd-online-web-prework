9377e6d95663fcf1666ed8e716e22f4561e8d22b
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-12 17:04:47 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+first_number = 3
+second_number = 7
+
+# Addition
+sum = first_number + second_number
+
+# Subtraction
+difference = first_number - second_number
+
+# Multiplication
+product = first_number * second_number
+
+# Division
+quotient = first_number / second_number
